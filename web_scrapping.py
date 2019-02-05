58227bc3eb6406db6560a32f6edfb562251d360e
GenedyMohamed <genedymohamed96@gmail.com>
2019-02-05 15:13:16 +0200

    cleaning code

--- web_scrapping.py
+++ web_scrapping.py
@@ -1,6 +1,7 @@
 from requests import get
 from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
+import unicodedata as ud
 
 def simple_get(url):
     # Attemps to get the content at 'url' by making an HTTP GET request.
@@ -38,11 +39,40 @@ def read_html(raw_html):
         html = BeautifulSoup(raw_html, 'html.parser')
     except:
         html = BeautifulSoup("404 Not Found!", 'html.parser')
-    print(type(html))
     return html#.__unicode__()
 
 def add_to_csv(year, title, link):
     f = open('data.csv', 'a', encoding="utf-8")
     f.write('\n'+year+', '+title+', '+link)
     f.close()
+
+#Check if word is in Latin alphabet or not.
+latin_letters= {}
+def is_latin(uchr):
+    try: return latin_letters[uchr]
+    except KeyError:
+         return latin_letters.setdefault(uchr, 'LATIN' in ud.name(uchr))
+def only_roman_chars(unistr):
+    return all(is_latin(uchr)
+           for uchr in unistr
+           if uchr.isalpha())
+
+def add_films_to_csv():
+    for i in range(77, 80): # get arabic movie names and links from year 1977 to 1979
+        print(i)
+        for j in range(1, 100):
+            raw_html = simple_get("https://www.elcinema.com/index/work/release_year/19{}?page={}".format(i, j))
+            print(j)
+            if(raw_html is None):
+                break
+            html = read_html(raw_html)
+            for td in html.select("td"):
+                a_tags = td.findAll("a", recursive=False)
+                for a in a_tags:
+                    if(a.find("img") and td.parent.find("td", string="فيلم")):
+                        movie = a.parent.findAll("a", recursive=False)[-1]
+                        movie_name = movie.text
+                        if(not only_roman_chars(movie_name)):
+                            movie_link = movie['href']
+                            add_to_csv("19"+str(i), movie_name, movie_link)
 
