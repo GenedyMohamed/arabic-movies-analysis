9497e73772e721d2e50e28034f231b6a3fd2b515
MohamedAboubasha <mohamedaboubasha@gmail.com>
2019-03-17 12:02:28 +0200

    doc2vec

--- word2vec.py
+++ word2vec.py
@@ -1,6 +1,9 @@
 #from gensim.models import Word2Vec
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 #from nltk import word_tokenize
+from sklearn.cluster import KMeans
+import numpy as np
+import matplotlib.pyplot as plt
 
 file = open('synopses.txt', 'r', encoding = 'utf-8')
 s = file.read()
@@ -20,18 +23,60 @@ for synopsis in synopses:
 
 #print(filtered_synopses[0].split('||')[0])
 
-documents = [TaggedDocument(synopsis_name.split('||')[1].strip(), [i]) for i, synopsis_name in enumerate(filtered_synopses)]
-model = Doc2Vec(documents, vector_size=1, window=1, min_count=1, workers=4)
-
-#print(documents[0], '\n', documents[1])
-print(model.docvecs.most_similar(positive = [493]))
-with open('SampleDoc2VecOutput.txt', 'w', encoding='utf-8') as file:
-    for item in model.docvecs.most_similar(positive=[493]):
-        file.write(model.docvecs.__getitem__(item[0]))
-        file.write('     ')
-        file.write(filtered_synopses[item[0]].split('||')[0])
+with open('test123.txt', 'w', encoding='utf-8') as file:
+    for item in filtered_synopses:
+        file.write(item.split('||')[0].strip())
+        
+        #print(item[0], '\t',item[1], '\n')
+        #file.write('     ')
+        #file.write(filtered_synopses[item[0]].split('||')[0])
+        #file.write(item[0])
         file.write('\n')
-    
+
+#documents = [TaggedDocument(synopsis_name.split('||')[1].strip(), [i]) for i, synopsis_name in enumerate(filtered_synopses)]
+documents = [TaggedDocument(synopsis_name.split('||')[1].strip().split(), [synopsis_name.split('||')[0].strip()]) for synopsis_name in filtered_synopses]
+model = Doc2Vec(documents, vector_size = 300, window=25, min_count=20, workers=4, epochs=20)
+
+#print(documents[0])
+
+
+#print(filtered_synopses[493].split('||')[0])
+#for item in model.docvecs.most_similar(positive = model.docvecs['الورشة']):
+#    print(item, '\n')
+print(model.docvecs['الورشة'])
+#print(model.docvecs)
+
+vectors = []
+
+for i in range(len(filtered_synopses)):
+    vectors.append(model.docvecs[filtered_synopses[i].split('||')[0].strip()])
+
+
+kmeans = KMeans(n_clusters=5, random_state=0).fit(vectors)
+
+print('Labels:  ', kmeans.labels_)#array([1, 1, 1, 0, 0, 0], dtype=int)
+x = kmeans.predict([model.docvecs['قلب المرأة']])
+print(x)
+'''
+fig = plt.figure()
+fig.suptitle('Data PCA', fontsize=20)
+plt.xlabel('xl', fontsize=18)
+plt.ylabel('x2', fontsize=16)
+plt.scatter(data['x1'], data['x2'])
+fig.savefig('Data_PCA.png')
+'''
+#print(model.infer_vector('قلب المرأة'))
+'''
+with open('SampleDoc2VecOutput2.txt', 'w', encoding='utf-8') as file:
+    for item in model.docvecs.most_similar(positive=[model.infer_vector('قلب المرأة')]):
+        file.write(str(model.docvecs.__getitem__(item[0])))
+        
+        print(item[0], '\t',item[1], '\n')
+        file.write('     ')
+        #file.write(filtered_synopses[item[0]].split('||')[0])
+        file.write(item[0])
+        file.write('\n')
+''' 
 
 '''for synopsis in synopses:
     list_of_sentences.append(word_tokenize(synopsis))
